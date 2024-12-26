30d49b4fbeda9a6ef33da8d86b758fbe1171ee66
Jason Caldwell <jman8851@gmail.com>
2024-12-26 18:34:06 -0500

    Copied the questions in answers.txt and marked answers with the **. Also ran the experiement and saw the outputs when running node. Corrected things that was suggested such as cloning the updated repo.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,27 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+/*"use strict"
+
+console.log(1 + 1);         // Output: 2
+console.log(1 - 1);          // Output: 0
+console.log(1 + "1");        // Output: 11
+                             // What? Why? Search for the answer
+
+console.log(3 - 4 * 5)       // Output: -17
+console.log((3 - 4) * 5);    // Output: -5
+                             // Remember order of precendence
+
+console.log(6 / 5);          // Output: 1.2
+console.log(6 % 5);          // Output: 1
+                             // Modulus return the remainder. This operator is extremely useful.
+
+console.log(1 == 1);         // Output: true
+console.log(1 == "1");       // Output: true
+console.log(1 === "1");      // Output: false
+
+console.log(1 || 2);         // Output: 1
+console.log(2 || 1);         // Output: 2
+console.log(1 && 2);         // Output: false
+console.log(1 && 1);         // Output: true
+*/
 
